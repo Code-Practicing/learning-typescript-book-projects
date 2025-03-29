10aaac99c54edc87e2837c0d190939b35afc8907
isabellediasr <isabellediasr1@gmail.com>
2025-03-29 17:09:59 -0300

    :tada: init: initial commit

---
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/original.js
@@ -0,0 +1,22 @@
+async function checkEmotion(knownEmotions, emotion, callback) {
+	// Simulate database processing time by waiting a second...
+	setTimeout(() => {
+		callback(knownEmotions.has(emotion));
+	}, 1000);
+}
+
+async function speak(knownEmotions, newEmotion, phrase, callback) {
+	checkEmotion(knownEmotions, newEmotion, (hasEmotion) => {
+		if (hasEmotion) {
+			callback(`"${phrase}" (${newEmotion})`);
+		} else {
+			callback(
+				undefined,
+				new Error(`Does not compute. I do not understand ${newEmotion}.`)
+			);
+		}
+	});
+}
+
+module.exports.checkEmotion = checkEmotion;
+module.exports.speak = speak;
